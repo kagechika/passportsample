4660a2deab559cefcdd4c00a0d1a662ee424364c
kagechika <ktkgchk1907s@gmail.com>
2014-10-06 07:49:45 +0000

    adding express template

--- app.js
+++ app.js
@@ -1,75 +1,65 @@
-//
-// setting express
-//
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var path = require('path');
-
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var methodOverride = require('method-override');
-var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var multer = require('multer');
-var errorHandler = require('errorhandler');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.set('port', process.env.PORT || 50000);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(methodOverride());
-app.use(session({ resave: true,
-                  saveUninitialized: true,
-                  secret: 'uwotm8' }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-    app.use(errorHandler());
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-app.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
-//
-// setting passport
-//
-var passport = require('passport'),
-    twStrategy = require('passport-twitter').Strategy;
+app.listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
 
-passport.use(new twStrategy({
-        consumerKey: "xUUgA2PeFR1mEJB98OdQgKULD",
-        consumerSecret: "iu6HUO20an46qV55diQtlvyj6mOfWIIqVoI11KAGyWaqzzrZ49",
-        callbackURL: "http://ec2-54-64-144-22.ap-northeast-1.compute.amazonaws.com/auth/twitter/callback"
-    },
-    function(token, tokenSecret, profile, done){
-        User.findOrCreate({}, function(err, user){
-        if(err){ return done(err); }
-        done(null, user);
-    });
-    }
-));
-
-//
-// routing
-//
-app.get("/auth/twitter", passport.authenticate("twitter"));
-app.get("/auth/twitter/callback", passport.authenticate("twitter", {
-    successRedirect: "/",
-    failureRedirect: "/login"
-}));
-
-console.log("hello node");
+module.exports = app;
